11470ad7b69bf44ce688ee810170ce52841e7500
Fatih Pehlivan <73739817+fatihpehlvan@users.noreply.github.com>
2023-02-15 21:46:31 +0300

    Delete Employee Recording System directory

--- Employee Recording System/src/TemporaryEmployee.cpp
+++
@@ -1,7 +0,0 @@
-#include "TemporaryEmployee.h"
-
-TemporaryEmployee::TemporaryEmployee(int employeeNumber, int employeeType1, string name, string surname, string title,
-                                     double salaryCoefficient, const string &birthDate,
-                                     const string &dateOfAppointment, int lengthService) : Employee(employeeNumber, employeeType1, name, surname, title, salaryCoefficient, birthDate, dateOfAppointment, lengthService) {
-
-}
